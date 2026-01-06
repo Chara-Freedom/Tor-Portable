ea2c6739607013bc3a8c1df3f98cd68d5dc9eac1
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2026-01-06 09:32:59 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -3,21 +3,16 @@ cd "$(dirname "$0")"
 lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
 systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
-options=("Change the mode to pro" "Change the mode to default" "Remove middle nodes")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Change the mode to pro")
-            cp ./change-mode/pro/torrc.txt torrc.txt
-            read -p "The mode was changed to pro."
-            ;;
-        "Change the mode to default")
-            cp ./change-mode/default/torrc.txt torrc.txt
-            read -p "The mode was changed to default."
-            ;;
-        "Remove middle nodes")
-            sed -i 's/MiddleNodes/#MiddleNodes/' torrc.txt
-            read -p "Middle nodes were removed"
-            ;;
-    esac
-done
+read -n 1 -p "Welcome to the mode control panel. Do you want to set the mode to pro (1), set the mode to default (2), or remove middle nodes (3)?" INP
+if [[ $INP = 1 ]]; then
+cp ./change-mode/pro/torrc.txt torrc.txt
+read -p "The mode was changed to pro."
+fi
+if [[ $INP = 2 ]]; then
+cp ./change-mode/default/torrc.txt torrc.txt
+read -p "The mode was changed to default."
+fi
+if [[ $INP = 3 ]]; then
+sed -i 's/MiddleNodes/#MiddleNodes/' torrc.txt
+read -p "Middle nodes were removed."
+fi
