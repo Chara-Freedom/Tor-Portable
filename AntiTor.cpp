558493ed2728c8b711f81256ef48f81bb7f715b3
Chara-Freedom <lolered@yandex.ru>
2026-01-20 14:38:06 +0300

    Add files via upload
    Paths handling

--- AntiTor.cpp
+++ AntiTor.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <windows.h>
+#include <filesystem>
 int main() {
+WCHAR path[MAX_PATH];
+GetModuleFileNameW(NULL, path, MAX_PATH);
+std::filesystem::path fullpath(path);
+std::filesystem::current_path(fullpath.remove_filename());
 _wchdir(L"./tor");
 std::system("start /min tor.exe -f ../torrc.txt");
 }
