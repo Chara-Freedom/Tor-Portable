0438a4df23edf975ec665d8b74a661edc15cb27c
Chara-Freedom <lolered@yandex.ru>
2026-01-22 00:37:57 +0300

    Update AntiTor.cpp
    This makes life just a little bit easier

--- AntiTor.cpp
+++ AntiTor.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <windows.h>
 #include <filesystem>
+using namespace std;
 int main() {
 WCHAR path[MAX_PATH];
 GetModuleFileNameW(NULL, path, MAX_PATH);
-std::filesystem::path fullpath(path);
-std::filesystem::current_path(fullpath.remove_filename());
+filesystem::path fullpath(path);
+filesystem::current_path(fullpath.remove_filename());
 FILE* pipe = _popen("@echo off & for %I in (VERSION*) do echo %~nxI", "r");
 char buffer[100];
-std::string UPD;
+string UPD;
 while (fgets(buffer, sizeof(buffer), pipe) != NULL) UPD += buffer;
 _pclose(pipe);
 _wchdir(L"./tor");
-std::system("start /min tor.exe -f ../torrc.txt");
+system("start /min tor.exe -f ../torrc.txt");
 }
