729a7554e6e979e0ffbe6843b3bd3a2531304aaf
Chara-Freedom <lolered@yandex.ru>
2026-01-22 07:31:04 +0300

    Add files via upload

--- AntiTor.cpp
+++ AntiTor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 #include <filesystem>
 using namespace std;
@@ -10,11 +11,13 @@ filesystem::current_path(fullpath.remove_filename());
 FILE* pipe = _popen("@echo off & for %I in (VERSION*) do echo %~nxI", "r");
 char buffer[100];
 string UPD;
-while (fgets(buffer, sizeof(buffer), pipe) != NULL) UPD += buffer;
+while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+buffer[strcspn(buffer, "\n")] = 0;
+UPD += buffer;
+}
 _pclose(pipe);
-string TESTYO = "ZeroOmegaOptions-RU.bak";
-string link = "https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/" + TESTYO;
-string filename = "./tor/" + TESTYO;
+string link = "https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/" + UPD;
+string filename = "./tor/" + UPD;
 URLDownloadToFile(NULL, link.c_str(), filename.c_str(), 0, NULL);
 _wchdir(L"./tor");
 system("start /min tor.exe -f ../torrc.txt");
