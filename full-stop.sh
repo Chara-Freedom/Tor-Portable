e71065572d1c321552961e8cce20b3657f073e8c
Chara-Freedom <lolered@yandex.ru>
2026-02-01 17:37:38 +0300

    Update full-stop.sh

--- full-stop.sh
+++ full-stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
+lsof -t ./tor/ld-linux-x86-64.so.2 | xargs -r kill
 systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
 read -p ""
