6ab26e909df670dbcd578cd82a1d301e8ec6a32f
Chara-Freedom <lolered@yandex.ru>
2026-02-08 18:56:31 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -12,27 +12,33 @@ echo
 if [[ $INP = 0 ]]; then
 cp ./change-mode/random-exit/torrc.txt torrc.txt
 read -n 1 -p "The mode was changed to random-exit. " INP
+echo
 fi
 if [[ $INP = 1 ]]; then
 cp ./change-mode/exit-1/torrc.txt torrc.txt
 read -n 1 -p "The mode was changed to exit-1. " INP
+echo
 fi
 if [[ $INP = 2 ]]; then
 cp ./change-mode/exit-2/torrc.txt torrc.txt
 read -n 1 -p "The mode was changed to exit-2. " INP
+echo
 fi
 if [[ $INP = 3 ]]; then
 cp ./change-mode/custom/torrc.txt torrc.txt
 touch ./change-mode/custom/trace
 read -n 1 -p "The mode was changed to custom. " INP
+echo
 fi
 if [[ $INP = 4 ]]; then
  if grep -q "#MiddleNodes" torrc.txt; then
  read -n 1 -p "Middle nodes are already not in use. " INP
+ echo
  fi
  if ! grep -q "#MiddleNodes" torrc.txt; then
  sed -i 's/MiddleNodes/#MiddleNodes/' torrc.txt
  read -n 1 -p "Middle nodes were removed. " INP
+ echo
  fi
 fi
 done
