9b251583d683f112969ae830dc628a8e921f796c
Chara-Freedom <lolered@yandex.ru>
2026-02-08 17:58:07 +0300

    Update change-mode.cmd
    Allow multiple mode changes (mainly because you can combine removing middle nodes with any other mode), will be hard to do in bash Linux because there is no GOTO

--- change-mode.cmd
+++ change-mode.cmd
@@ -9,36 +9,32 @@ timeout /t 3 /nobreak
 Echo Welcome to the mode control panel.
 Echo.
 Echo Do you want to set the mode to random-exit (0, default), set the mode to exit-1 (1), set the mode to exit-2 (2), set the mode to custom (3), or remove middle nodes (4, applies to any other mode)?
+:Choice
 choice /c 01234 /n
 if %errorlevel% EQU 1 (
 copy "%CD%\change-mode\random-exit\torrc.txt" "%CD%\torrc.txt"
 echo The mode was changed to random-exit.
-pause
 )
 if %errorlevel% EQU 2 (
 copy "%CD%\change-mode\exit-1\torrc.txt" "%CD%\torrc.txt"
 echo The mode was changed to exit-1.
-pause
 )
 if %errorlevel% EQU 3 (
 copy "%CD%\change-mode\exit-2\torrc.txt" "%CD%\torrc.txt"
 echo The mode was changed to exit-2.
-pause
 )
 if %errorlevel% EQU 4 (
 copy "%CD%\change-mode\custom\torrc.txt" "%CD%\torrc.txt"
 type nul > "%CD%\change-mode\custom\trace"
 echo The mode was changed to custom.
-pause
 )
 if %errorlevel% EQU 5 (
 findstr /c:"#MiddleNodes" torrc.txt
 if !errorlevel! EQU 0 (
 echo Middle nodes are already not in use.
-pause
-exit
+GOTO Choice
 )
 powershell -Command " (gc """%CD%\torrc.txt""") -replace 'MiddleNodes', '#MiddleNodes' | Out-File """%CD%\torrc.txt""" -encoding default
 echo Middle nodes were removed.
-pause
 )
+GOTO Choice
