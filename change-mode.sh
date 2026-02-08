0e6d07e3284098360d0f3d80ae47a7653f3be31c
Chara-Freedom <lolered@yandex.ru>
2026-02-08 18:24:26 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -8,29 +8,25 @@ echo
 echo "Do you want to set the mode to random-exit (0, default), set the mode to exit-1 (1), set the mode to exit-2 (2), set the mode to custom (3), or remove middle nodes (4, applies to any other mode)?"
 read -n 1 -p "" INP
 while [[ -n $INP ]]; do
-if [[ $INP = 0 ]]; then
 echo
+if [[ $INP = 0 ]]; then
 cp ./change-mode/random-exit/torrc.txt torrc.txt
 read -p "The mode was changed to random-exit."
 fi
 if [[ $INP = 1 ]]; then
-echo
 cp ./change-mode/exit-1/torrc.txt torrc.txt
 read -p "The mode was changed to exit-1."
 fi
 if [[ $INP = 2 ]]; then
-echo
 cp ./change-mode/exit-2/torrc.txt torrc.txt
 read -p "The mode was changed to exit-2."
 fi
 if [[ $INP = 3 ]]; then
-echo
 cp ./change-mode/custom/torrc.txt torrc.txt
 touch ./change-mode/custom/trace
 read -p "The mode was changed to custom."
 fi
 if [[ $INP = 4 ]]; then
-echo
  if grep -q "#MiddleNodes" torrc.txt; then
  read -p "Middle nodes are already not in use."
  fi
