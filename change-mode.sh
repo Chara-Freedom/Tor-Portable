9b23ed86397c5f1fb5100630c0868788607982fc
Chara-Freedom <lolered@yandex.ru>
2026-02-09 04:07:09 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -8,36 +8,36 @@ echo
 read -n 1 -p "Do you want to set the mode to random-exit (0, default), set the mode to exit-1 (1), set the mode to exit-2 (2), set the mode to custom (3), or remove middle nodes (4, applies to any other mode)? " INP
 echo
 while [[ -n $INP ]]; do
-if [[ $INP = 0 ]]; then
+case $INP in
+0)
 cp ./change-mode/random-exit/torrc.txt torrc.txt
 read -n 1 -p "The mode was changed to random-exit. " INP
-echo
-fi
-if [[ $INP = 1 ]]; then
+echo;;
+1)
 cp ./change-mode/exit-1/torrc.txt torrc.txt
 read -n 1 -p "The mode was changed to exit-1. " INP
-echo
-fi
-if [[ $INP = 2 ]]; then
+echo;;
+2)
 cp ./change-mode/exit-2/torrc.txt torrc.txt
 read -n 1 -p "The mode was changed to exit-2. " INP
-echo
-fi
-if [[ $INP = 3 ]]; then
+echo;;
+3)
 cp ./change-mode/custom/torrc.txt torrc.txt
 touch ./change-mode/custom/trace
 read -n 1 -p "The mode was changed to custom. " INP
+echo;;
+4)
+if grep -q "#MiddleNodes" torrc.txt; then
+read -n 1 -p "Middle nodes are already not in use. " INP
 echo
 fi
-if [[ $INP = 4 ]]; then
- if grep -q "#MiddleNodes" torrc.txt; then
- read -n 1 -p "Middle nodes are already not in use. " INP
- echo
- fi
- if ! grep -q "#MiddleNodes" torrc.txt; then
- sed -i 's/MiddleNodes/#MiddleNodes/' torrc.txt
- read -n 1 -p "Middle nodes were removed. " INP
- echo
- fi
-fi
+if ! grep -q "#MiddleNodes" torrc.txt; then
+sed -i 's/MiddleNodes/#MiddleNodes/' torrc.txt
+read -n 1 -p "Middle nodes were removed. " INP
+echo
+fi;;
+*)
+exec "$0"
+exit;;
+esac
 done
