27d5e1bb16a1a23d56dc73bb13af6d3ba189c2e6
Chara-Freedom <rabcristine@proton.me>
2026-02-13 03:38:13 +0300

    Update ipns-republisher.sh

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -7,7 +7,7 @@ while pgrep -x ld-linux-x86-64 > /dev/null
 do
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 record.bin --force
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/k51qzi5uqu5dhr0t6o7hfymktwipat1243r24wqzfrbhery1z4f85alzgq12pt > record.bin
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put k51qzi5uqu5dhr0t6o7hfymktwipat1243r24wqzfrbhery1z4f85alzgq12pt record.bin --force
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/link2 > record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put link2 record.bin --force
 sleep 1d
 done
