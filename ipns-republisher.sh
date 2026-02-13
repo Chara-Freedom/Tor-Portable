d3433d7065b5e304ca8f3168cfa69a0bf112bad3
Chara-Freedom <rabcristine@proton.me>
2026-02-13 03:36:18 +0300

    Update ipns-republisher.sh
    The new built-in republisher from kubo 0.40 RC1

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -5,9 +5,9 @@ cd "$(dirname "$0")"
 sleep 20
 while pgrep -x ld-linux-x86-64 > /dev/null
 do
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
-curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/link > record.bin
-curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 record.bin --force
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/k51qzi5uqu5dhr0t6o7hfymktwipat1243r24wqzfrbhery1z4f85alzgq12pt > record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put k51qzi5uqu5dhr0t6o7hfymktwipat1243r24wqzfrbhery1z4f85alzgq12pt record.bin --force
 sleep 1d
 done
