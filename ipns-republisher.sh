188326703b7955b8f92086579fcaf051b1a03e09
Chara-Freedom <rabcristine@proton.me>
2026-02-21 12:52:52 +0300

    Update ipns-republisher.sh
    0.40 release format

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -6,8 +6,8 @@ sleep 20
 while pgrep -x ld-linux-x86-64 > /dev/null
 do
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 record.bin --force
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 record.bin
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/link2 > record.bin
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put link2 record.bin --force
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put link2 record.bin
 sleep 1d
 done
