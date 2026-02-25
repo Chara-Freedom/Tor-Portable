1eb504c318911fdb7fdc004bf5ca54eca1a72429
Chara-Freedom <rabcristine@proton.me>
2026-02-25 09:47:48 +0300

    Update AntiTor.sh

--- AntiTor.sh
+++ AntiTor.sh
@@ -2,11 +2,14 @@
 cd "$(dirname "$0")"
 UPD=(VERSION*)
 curl "https://k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4.ipns.dweb.link/$UPD" -f -s -o /dev/null
-if [ $? -eq 22 ]; then
+if [[ $? = 22 && ! -f "./AUTO.no" ]]; then
 read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to skip " INP
 echo
  if [ $INP != 0 ]; then
  ./updater.sh
+ fi
+ if [ $INP = 0 ]; then
+ touch "./AUTO.no"
  fi
 fi
 cd tor
