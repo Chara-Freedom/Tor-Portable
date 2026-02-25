e62f1f31fade51b12b900bb6e3e3a8cd78eac728
Chara-Freedom <rabcristine@proton.me>
2026-02-25 09:39:01 +0300

    Update service-manager.sh
    Correct syntax

--- service-manager.sh
+++ service-manager.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 UPD=(VERSION*)
 curl "https://k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4.ipns.dweb.link/$UPD" -f -s -o /dev/null
-if [[ $? -eq 22 && ! -f "./AUTO.no" ]]; then
+if [[ $? = 22 && ! -f "./AUTO.no" ]]; then
 read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to disable autoupdate (delete AUTO.no to enable again) " INP
 echo
 systemctl --user is-active --quiet tor.service
