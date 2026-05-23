1bea52fc0a50d240163a8a5118cffb96cb917877
Chara-Freedom <rabcristine@proton.me>
2026-05-23 20:40:57 +0300

    Update autostart.cmd

--- autostart.cmd
+++ autostart.cmd
@@ -1,4 +1,4 @@
-@echo off
+@echo off & cd /d "%~dp0"
 if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\AntiTor.lnk" (
 del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\AntiTor.lnk"
 echo Windows autostart removed.
@@ -6,5 +6,6 @@ pause
 exit
 )
 powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\AntiTor.lnk'); $s.TargetPath='%CD%\AntiTor.exe'; $s.Save()"
+start "" "%CD%\AntiTor.exe"
 Echo Windows autostart created.
 pause
